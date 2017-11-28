27cef1f5ca770a2a18b4274d347b111fe97bf390
MathisPaturle <MathisPaturle@MATHISPATURAC9A>
2017-11-28 10:34:17 +0100

    XML AA07

---
+++ GameProject/XMLTest/main.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <sstream> 
+#include <streambuf>
+#include <vector>
+
+#include "../../GameProject/dep/inc/XML/rapidxml.hpp"
+#include "../../GameProject/dep/inc/XML/rapidxml_utils.hpp"
+#include "../../GameProject/dep/inc/XML/rapidxml_iterators.hpp"
+#include "../../GameProject/dep/inc/XML/rapidxml_print.hpp"
+
+
+struct Enemy {
+
+	std::string name;
+	int vitality;
+	int damageAttack;
+	int defense;
+	float frequencyAttack;
+	int experience;
+
+};
+
+int main() {
+
+	std::vector<Enemy> enemies;
+	//Creación de un documento para poder leer su contenido
+	rapidxml::xml_document<> doc;
+	std::ifstream file("../../GameProject/res/files/config.xml"); 
+	std::stringstream buffer;
+	
+	buffer << file.rdbuf();
+	file.close();
+	std::string content(buffer.str()); doc.parse<0>(&content[0]);
+
+	//Ejemplo: Imprimiendo la información de la raíz del documento
+	std::cout << "Nombre de la raiz: " << doc.first_node()->name() << "\n\n"<< std::endl; 
+	//std::cout << "Value: " << doc.first_node()->value() << std::endl;
+
+	//Ejemplo: Imprimiendo para todos los eldleiuementos "book" del documento su valor (text content) (no se están considerando a los atributos)
+	rapidxml::xml_node<> *pRoot = doc.first_node();
+	for (rapidxml::xml_node<> *pNode = pRoot->first_node("enemy"); pNode; pNode = pNode->next_sibling()) {
+		
+		
+		Enemy e;
+		e.name = pNode->first_attribute("name")->value();
+		e.vitality = std::stoi(pNode->first_attribute("vitality")->value());
+		e.damageAttack = std::stoi(pNode->first_attribute("damageAttack")->value());
+		e.defense = std::stoi(pNode->first_attribute("defense")->value());
+		e.frequencyAttack = std::stoi(pNode->first_attribute("frequencyAttack")->value());
+		e.experience = std::stoi(pNode->first_attribute("experience")->value());
+
+		enemies.push_back(e);
+
+
+
+		/*for (const rapidxml::xml_attribute<>* attr = pNode->first_attribute(); attr; attr = attr->next_attribute()) {
+			std::cout << "- " << attr->name() << ": " << attr->value() << std::endl;
+		}*/	
+		//std::cout << pNode->name() << ": "<< enemies[count].name <<std::endl;
+		/*for (rapidxml::xml_node<> *pNodeI = pNode->first_node(); pNodeI; pNodeI = pNodeI->next_sibling()){
+			std::cout <<"- " << pNodeI->name() << ": " << pNodeI->value() << std::endl;
+		};*/
+
+	}
+
+	for (std::vector<Enemy>::iterator it = enemies.begin(); it != enemies.end(); ++it) {
+
+		std::cout << "- Name: " << it->name << std::endl;
+		std::cout << "- Vitality: " << it->vitality << std::endl;
+		std::cout << "- Damage Attack: " << it->damageAttack << std::endl;
+		std::cout << "- Defense: " << it->defense << std::endl;
+		std::cout << "- Frequency Attack: " << it->frequencyAttack << std::endl;
+		std::cout << "- Experience: " << it->experience << std::endl;
+		std::cout <<"=============================================\n"<< std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
